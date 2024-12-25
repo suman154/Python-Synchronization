f044c13f6efa156e299c8eb4197936d3c28f2704
Suman Bhatta <sumanbhatta735@gmail.com>
2024-12-25 20:25:39 +0545

    Add examples of thread interruption using a flag and an event object

---
+++ Interrupting a Thread/Interruption_using_event_object.py
@@ -0,0 +1,28 @@
+from time import sleep
+from threading import Thread
+from threading import Event
+
+class MyThread(Thread):
+   def __init__(self, event):
+      super(MyThread, self).__init__()
+      self.event = event
+
+   def run(self):
+      i=0
+      while True:
+         i+=1
+         print ('Child thread running...',i)
+         sleep(0.5)
+         if self.event.is_set():
+            break
+         print()
+      print('Child Thread Interrupted')
+
+event = Event()
+thread1 = MyThread(event)
+thread1.start()
+
+sleep(3)
+print('Main thread stopping child thread')
+event.set()
+thread1.join()
